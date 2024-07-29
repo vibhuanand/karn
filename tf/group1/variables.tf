7e644a9b604c8474d78c4fe302ffb3bc0109ed70
Vibhu <vibhuanand@outlook.com>
2024-07-29 13:53:37 -0400

    add permissions to workflow

--- tf/group1/variables.tf
+++ tf/group1/variables.tf
@@ -1,17 +1,17 @@
 variable "environment" {
   type        = string
   description = "(Requierd) Name of the resource group"
-  default = "sand"
+  default     = "sand"
 }
 
 variable "resource_group_name" {
   type        = string
   description = "(Requierd) Name of the resource group"
-  default = "karn-rg-{{ var.environment }}"
+  default     = "karn-rg-{{ var.environment }}"
 }
 
 variable "location" {
   type        = string
   description = "(Requierd) Location of the resources"
-  default = "Central India"
+  default     = "Central India"
 }
