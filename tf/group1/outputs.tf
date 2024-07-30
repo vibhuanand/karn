2a4dc54787244fc62b5bc7bf3e958f0e85f8084a
Vibhu <vibhuanand@outlook.com>
2024-07-30 15:38:06 -0400

    fix output

--- tf/group1/outputs.tf
+++ tf/group1/outputs.tf
@@ -2,6 +2,6 @@ output "static_site_id" {
   value = azurerm_static_site.static_site.id
 }
 
-output "static_site_default_hostname" {
-  value = azurerm_static_site.static_site.default_hostname
+output "static_site_default_host_name" {
+  value = azurerm_static_site.static_site.default_host_name
 }
