545450cf564568df2aee4ab8b8dd8c7ac1d5e239
Vibhu <vibhuanand@outlook.com>
2024-08-04 16:18:50 -0400

    rm tf.vars

--- tf/group1/main.tf
+++ tf/group1/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "rg" {
 
 
 resource "azurerm_static_web_app" "static_web_app" {
-  name                = "karn-static-webapp-${local.environment}"
+  name                = var.resource_group_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 
--- tf/group1/variables.tf
+++ tf/group1/variables.tf
@@ -3,14 +3,17 @@
 variable "environment" {
   type        = string
   description = "(Required) Environment for the resource group"
+  default = "sand"
 }
 
 variable "resource_group_name" {
   type        = string
   description = "(Required) Name of the resource group"
+  default = "karn-static-webapp-${local.environment}"
 }
 
 variable "location" {
   type        = string
   description = "(Required) Location of the resources"
+  default = "Central India"
 }
