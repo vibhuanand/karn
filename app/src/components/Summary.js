3e9b8d2b4823e1274c80c85c7f68e1b10adf58c3
Vibhu <vibhuanand@outlook.com>
2024-08-12 08:23:50 -0400

    add summary

---
+++ app/src/components/Summary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Summary() {
+  return (
+    <div>
+      <h2>Summary</h2>
+      {/* Implement your summary logic here */}
+    </div>
+  );
+}
+
+export default Summary;
