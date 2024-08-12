75fa74e20a3bf9ecc5a3f55f4d9a37e78ef09ca9
Vibhu <vibhuanand@outlook.com>
2024-08-12 11:13:01 -0400

    add summary

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Add from './components/Add';
 import Estimate from './components/Estimate';
 import Subtract from './components/Subtract';
-import Summary from './components/summary';
+// import Summary from './components/Summary';
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
             <li><Link to="/Add">Add</Link></li>
             <li><Link to="/Estimate">Estimate</Link></li>
             <li><Link to="/Subtract">Subtract</Link></li>
-            <li><Link to="/Summary">Summary</Link></li>
+            {/* <li><Link to="/Summary">Summary</Link></li> */}
           </ul>
         </nav>
         <div className="content">
@@ -22,7 +22,7 @@ function App() {
             <Route path="/Add" component={Add} />
             <Route path="/Estimate" component={Estimate} />
             <Route path="/Subtract" component={Subtract} />
-            <Route path="/Summary" component={Summary} />
+            {/* <Route path="/Summary" component={Summary} /> */}
             <Route path="/" exact component={Add} />
           </Switch>
         </div>
