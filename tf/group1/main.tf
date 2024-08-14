4b76cb792d4bdd4ea1bd63608677393aad63dd5d
Vibhu <vibhuanand@outlook.com>
2024-08-14 15:51:43 -0400

    deploy sql

--- tf/group1/main.tf
+++ tf/group1/main.tf
@@ -16,3 +16,58 @@ resource "azurerm_static_web_app" "static_web_app" {
 
   sku_tier = "Free"
 }
+
+# provider.tf
+provider "azurerm" {
+  features {}
+}
+
+# main.tf
+resource "azurerm_resource_group" "rg" {
+  name     = "karn-sql-rg"
+  location = "Central India"
+}
+
+resource "azurerm_sql_server" "sql_server" {
+  name                         = "karnsqlserver"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = "P@ssword1234"
+}
+
+resource "azurerm_sql_database" "sql_db" {
+  name                = "karnsqldb"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  server_name         = azurerm_sql_server.sql_server.name
+
+  sku {
+    name     = "S0"
+    capacity = 10
+    tier     = "Standard"
+    family   = "Gen5"
+  }
+}
+
+# outputs.tf
+output "sql_server_name" {
+  value = azurerm_sql_server.sql_server.name
+}
+
+output "sql_database_name" {
+  value = azurerm_sql_database.sql_db.name
+}
+
+output "sql_admin_username" {
+  value = azurerm_sql_server.sql_server.administrator_login
+}
+
+output "sql_admin_password" {
+  value = azurerm_sql_server.sql_server.administrator_login_password
+}
+
+output "sql_connection_string" {
+  value = "Server=tcp:${azurerm_sql_server.sql_server.name}.database.windows.net,1433;Initial Catalog=${azurerm_sql_database.sql_db.name};Persist Security Info=False;User ID=${azurerm_sql_server.sql_server.administrator_login};Password=${azurerm_sql_server.sql_server.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+}
