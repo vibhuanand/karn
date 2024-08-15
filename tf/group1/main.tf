d367c88208a0febf412f1ca1cfe449de67b71e98
Vibhu <vibhuanand@outlook.com>
2024-08-15 14:15:40 -0400

    add sql

--- tf/group1/main.tf
+++ tf/group1/main.tf
@@ -10,39 +10,3 @@ resource "azurerm_static_web_app" "static_web_app" {
 
   sku_tier = "Free"
 }
-
-resource "azurerm_mssql_server" "sqlserver" {
-  name                         = "karn-sql-server-${var.environment}"
-  resource_group_name          = var.resource_group_name
-  location                     = azurerm_resource_group.rg.location
-  version                      = "12.0"
-  administrator_login          = "karn-admin"
-  administrator_login_password = var.sql_admin_password
-  minimum_tls_version          = "1.2"
-
-  tags = {
-    environment = "production"
-  }
-}
-
-resource "azurerm_mssql_database" "sqldatabase" {
-  name                = "karn-base-sql-database-${var.environment}"
-  resource_group_name = var.resource_group_name
-  location            = azurerm_resource_group.rg.location
-  server_name         = azurerm_sql_server.sqlserver.name
-
-  sku_name = "Basic"
-
-  max_size_bytes = "1073741824" # 1 GB
-  collation      = "SQL_Latin1_General_CP1_CI_AS"
-
-  tags = {
-    environment = var.environment
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-
