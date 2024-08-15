cdb6eef325df9a7e4ddd220e047b64b6a29cd1b4
Vibhu <vibhuanand@outlook.com>
2024-08-15 14:27:10 -0400

    add sql

--- tf/group1/sql.tf
+++ tf/group1/sql.tf
@@ -6,8 +6,6 @@ resource "azurerm_sql_server" "sql_server" {
   version                       = "12.0"
   administrator_login           = "karn-admin"
   administrator_login_password  = var.sql_admin_password
-  minimum_tls_version           = "1.2"
-  public_network_access_enabled = true
 
   tags = {
     environment = var.environment
