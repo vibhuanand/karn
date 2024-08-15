d6ad84c305d5e12f7768dfa5c9f1b2abf8ef9a9c
Vibhu <vibhuanand@outlook.com>
2024-08-15 14:24:45 -0400

    add sql

---
+++ tf/group1/sql.tf
@@ -0,0 +1,30 @@
+
+resource "azurerm_sql_server" "sql_server" {
+  name                          = "karn-sql-server-${var.environment}"
+  resource_group_name           = azurerm_resource_group.rg.name
+  location                      = azurerm_resource_group.rg.location
+  version                       = "12.0"
+  administrator_login           = "karn-admin"
+  administrator_login_password  = var.sql_admin_password
+  minimum_tls_version           = "1.2"
+  public_network_access_enabled = true
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "azurerm_sql_database" "sql_database" {
+  name                = "karn-sql-database-${var.environment}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  server_name         = azurerm_sql_server.sql_server.name
+  collation           = "SQL_Latin1_General_CP1_CI_AS"
+  max_size_bytes      = 1073741824 // 1 GB
+
+  read_scale = false
+
+  tags = {
+    environment = var.environment
+  }
+}
