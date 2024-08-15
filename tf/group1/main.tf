5101e308a7198ea6443da9d5e06a1944ee47eeb7
Vibhu <vibhuanand@outlook.com>
2024-08-15 16:33:10 -0400

    add function

--- tf/group1/main.tf
+++ tf/group1/main.tf
@@ -10,3 +10,46 @@ resource "azurerm_static_web_app" "static_web_app" {
 
   sku_tier = "Free"
 }
+
+resource "azurerm_storage_account" "function_storage_account" {
+  name                     = "st-function-${var.environment}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = "aps-${var.environment}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "FunctionApp"
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+# Azure Function App
+resource "azurerm_function_app" "example" {
+  name                       = "add-function-app-${var.environment}"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  app_service_plan_id        = azurerm_app_service_plan.app_service_plan.id
+  storage_account_name       = azurerm_storage_account.function_storage_account.name
+  storage_account_access_key = azurerm_storage_account.function_storage_account.primary_access_key
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  site_config {
+    always_on = false
+  }
+
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME" = "python"
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+  }
+}
